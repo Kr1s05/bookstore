366986c613d008768a580eb89ee15a727f4a0267
Kr1s05 <kristianoreshkov04@gmail.com>
2023-05-03 09:08:54 +0300

    Objects only

--- src/main/java/com/example/bookstore/HomeController.java
+++ src/main/java/com/example/bookstore/HomeController.java
@@ -1,7 +1,10 @@
 package com.example.bookstore;
 
+import com.example.bookstore.user.User;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 
 @Controller
@@ -9,6 +12,12 @@ public class HomeController {
 
     @GetMapping("/")
     public String index() {
+        return "home/index";
+    }
+
+    @RequestMapping("/register")
+    public String register(@ModelAttribute("user") User user){
+
         return "home/index";
     }
 
