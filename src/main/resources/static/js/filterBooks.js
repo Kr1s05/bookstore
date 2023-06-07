72a511c5f9fd50f3b46a06a26891449d2d6136c3
Georgi <19303@uktc-bg.com>
2023-06-07 10:55:55 +0300

    separated the filter script

---
+++ src/main/resources/static/js/filterBooks.js
@@ -0,0 +1,65 @@
+$("#applyFilters").click(function () {
+    let authors = [];
+    $('[id$="Author"]:checked').each(function() {
+        let author = $(this).val();
+        authors.push(author);
+    });
+
+    // Collect selected genres
+    let genres = [];
+    $('[id$="Genre"]:checked').each(function() {
+        let genre = $(this).val();
+        genres.push(genre);
+    });
+
+    // Get min and max price values
+    const minPrice = $('#MinPrice').val();
+    const maxPrice = $('#MaxPrice').val();
+
+    const ulElement = document.getElementById('pages');
+    let activeId = '1';
+    if (ulElement) {
+        const activeElement = ulElement.querySelector('.active');
+        if (activeElement) {
+            activeId = activeElement.id;
+        }
+    }
+    let pageNumber = parseInt(activeId);
+
+    const token = $("meta[name='_csrf']").attr("content");
+    const header = $("meta[name='_csrf_header']").attr("content");
+
+    $(document).ajaxSend(function(e, xhr) {
+        xhr.setRequestHeader(header, token);
+    });
+    console.log('sending request...');
+    console.log(JSON.stringify({
+        authors: authors,
+        genres: genres,
+        minPrice: minPrice,
+        maxPrice: maxPrice,
+        pageNumber: pageNumber,
+        pageSize: 16
+    }));
+    $.ajax({
+        url: "/books",
+        method: "POST",
+        contentType: "application/json",
+        data: JSON.stringify({
+            authors: authors,
+            genres: genres,
+            minPrice: minPrice,
+            maxPrice: maxPrice,
+            pageNumber: pageNumber,
+            pageSize: 16
+        }),
+        success: function(data) {
+            console.log("Request successful");
+            $("#contentBox").html(data);
+            console.log(data);
+        },
+        error: function(xhr, status, error) {
+            console.log("Request failed: " + error);
+        }
+    });
+});
